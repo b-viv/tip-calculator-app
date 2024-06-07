9b8a7d5e865f0bdabbc14e65cb774d655cde357e
b-vivien <134312026+b-viv@users.noreply.github.com>
2024-06-07 22:23:20 +0200

    calculator.component.ts update

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -4,7 +4,7 @@ import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.sass'
+  styleUrls: ['./calculator.component.sass']
 })
 
 export class CalculatorComponent {
@@ -21,26 +21,29 @@ export class CalculatorComponent {
   selectTip(tip: number) {
     this.selectedTipInput = tip;
     this.selectedTip = tip;
+    this.isValidInputZero();
   }
 
   get tipPerPerson(): number {
-    if (this.selectedTipInput > 0) {
-      return ((this.billInput * this.selectedTipInput) /100) / this.personInput;
-    }else if (this.customTipInput > 0) {
-      return ((this.billInput * this.customTipInput) /100) / this.personInput;
-    } else {
-      return 0;
+    if (this.personInput > 0 && this.billInput > 0) {
+      if (this.selectedTipInput > 0) {
+        return ((this.billInput * this.selectedTipInput) / 100) / this.personInput;
+      } else if (this.customTipInput > 0) {
+        return ((this.billInput * this.customTipInput) / 100) / this.personInput;
+      }
     }
+    return 0;
   }
 
   get totalPerPerson(): number {
-    if (this.selectedTipInput > 0) {
-      return (((this.billInput * this.selectedTipInput) /100) + this.billInput) / this.personInput;
-    }else if (this.customTipInput > 0) {
-      return (((this.billInput * this.customTipInput) /100) + this.billInput) / this.personInput;
-    } else {
-      return 0;
+    if (this.personInput > 0 && this.billInput > 0) {
+      if (this.selectedTipInput > 0) {
+        return (((this.billInput * this.selectedTipInput) / 100) + this.billInput) / this.personInput;
+      } else if (this.customTipInput > 0) {
+        return (((this.billInput * this.customTipInput) / 100) + this.billInput) / this.personInput;
+      }
     }
+    return 0;
   }
 
   isValidInputZero(): void {
@@ -51,12 +54,11 @@ export class CalculatorComponent {
   reset(): void {
     this.billInput = 0;
     this.selectedTipInput = 0;
-    this.customTipInput;
+    this.customTipInput = 0;
     this.personInput = 0;
     this.isBillZero = false;
     this.isPersonZero = false;
     this.selectedTip = null;
     this.isClicked = !this.isClicked;
   }
-
 }
